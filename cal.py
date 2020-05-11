727d40f139103b80258b4fbf1135198c1b9b598f
tobybrave <leutoby@gmail.com>
2020-05-11 12:46:52 +0100

    Error handling for character input

--- cal.py
+++ cal.py
@@ -1,2 +1,11 @@
-r = eval(input("Enter the radius:"))
-print("The area of the circle is",3.142*(r**2))
+cal = True
+while cal:
+		try:
+			r = eval(input("Enter the radius in (cm):"))
+		except NameError:
+			print("Only digits accepted!!")
+			
+		else:
+			area = 3.142 * ( r ** 2)
+			print(f"The area of the circle is {area :.3f}cm")
+			cal = False
